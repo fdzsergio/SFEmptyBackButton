1a249389afaf9d74c6803baf678ec851fb360dd3
Sergio Fernández <fdz.sergio@gmail.com>
2015-12-13 20:40:51 +0100

    added some documentation to header class

--- Classes/SFEmptyBackButton.h
+++ Classes/SFEmptyBackButton.h
@@ -16,7 +16,26 @@ FOUNDATION_EXPORT const unsigned char SFEmptyBackButtonVersionString[];
 
 @interface SFEmptyBackButton : NSObject
 
+/**
+ `SFEmptyBackButton` is a useful library to remove text from back button and align the title in the center of 
+ `UIViewController`.
+ */
+
+/**
+ By default, invoke `removeTitleFromAllViewControllers` to remove all titles from `ViewControllers`.
+
+ */
 + (void)removeTitleFromAllViewControllers;
+
+
+/**
+ If you want remove back title from selected `ViewControllers` invoke this method with ViewController.class array that 
+ you want.
+
+ @param array with viewControllers to empty back title
+
+ @warning set only (NSArray<UIViewController *> *) on parameter
+ */
 + (void)removeTitleFromViewControllers:(NSArray *)viewControllers;
 
 @end
